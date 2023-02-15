ee1083bd54cab80d366d2164b1404b10d8094be9
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-02-15 22:19:53 +0530

    added add-todo

--- src/Components/AddTodo.js
+++ src/Components/AddTodo.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function AddTodo() {
   return (
-    <div>AddTodo</div>
+    <div className="my-3 p-4">
+        <input type="text" className="form-control w-100" id="exampleFormControlInput1" placeholder="Enter a new task"/>
+    </div>
   )
 }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,13 +1,12 @@
 import React from 'react'
+import AddTodo from './AddTodo'
 import TodoItem from './TodoItem'
 
 export default function Main({todos, onDelete}) {
   return (
     <div className='container-sm mt-5'>
         <h3 className='text-center mb-4'>Welcome, here are your tasks for today.</h3>
-        <div className="my-3 p-4">
-          <input type="text" className="form-control w-100" id="exampleFormControlInput1" placeholder="Enter a new task"/>
-        </div>
+        <AddTodo/>
         {todos.length === 0 ? <center>Nothing to see here.</center> :
         todos.map((todo) => {
           return (<TodoItem task={todo} key={todo.sno} onDelete={onDelete}/>)
