89a915a367478c4b614804abfa298f81006e0670
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-06-14 15:34:33 +0530

    Updated README.md

--- frontend/src/Components/Main.js
+++ frontend/src/Components/Main.js
@@ -27,11 +27,13 @@ export default function Main({tasks, addTodo, deleteAll}) {
         <h3 className='text-center mb-4'>Welcome, here are your tasks for today.</h3>
         <AddTodo addTodo={addTodo} deleteAll={deleteAll}/> 
         <br/>
+        <div className='d-flex flex-column flex-column-reverse'>
         {typeof data.tasks === 'undefined' ? (<center>Loading...</center>) :
         (data.tasks.map((task) => {
           return (<TodoItem task={task} key={task.sno} onDelete={onDelete}/>)
-        }).reverse())
+        }))
         }
+        </div>
         <div className='m-5'>
             <br/>
             <br/>
